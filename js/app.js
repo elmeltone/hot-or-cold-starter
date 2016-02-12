0f24ea769656dedabbdc45c6ce062ac6b6a09f6f
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-11 19:29:26 -0800

    hot-cold

--- js/app.js
+++ js/app.js
@@ -13,26 +13,27 @@ $(document).ready(function(){
   	});
 
 	/*--- New Game ---*/
-    $(".new").click(function(){
-    	targetValue = Math.floor(Math.random() * 100) + 1;;
-		//console.log(targetValue);
-    });
+	$(".new").click(function(){
+	    	targetValue = Math.floor(Math.random() * 100) + 1;
+			console.log(targetValue);
+
+			/*--- Guess ---*/
+			$(".button").on('click',(function(){
+			var counter = $("count").value;
+			console.log(counter);
+			var input = document.getElementById("userGuess").value;
 	
-	/*--- Guess ---*/
-	$("#guessButton").on('click'(function(){
-		var counter = document.getElementById("count").value;
-		var input = document.getElementById("userGuess").value;
-		
-		return counter + 1;
-      	if (input === targetValue)
-		  	alert("You guessed it!");
-		else if (((input > targetValue) && (input - targetValue <= 10)) || (input < targetValue) && (input - targetValue >= -10)
-			alert("Ow! Hot! Try again!");
-		else if (((input > targetValue) && (input - targetValue > 10)) || (input < targetValue) && (input - targetValue < -10)
-			alert("Brrrr! Cold. Try again!");
-	}));
-	
-	
+			//return counter + 1;
+  			if (input === targetValue)
+	  			alert("You guessed it!");
+			else if (((input > targetValue) && (input - targetValue <= 10)) || ((input < targetValue) && (input - targetValue >= -10)))
+				alert("Ow! Hot! Try again!");
+			else if (((input > targetValue) && (input - targetValue > 10)) || ((input < targetValue) && (input - targetValue < -10)))
+				alert("Brrrr! Cold. Try again!");
+		}));
+
+
+	});
 	
 });
 
